bd480cc0f9e070e1ffc3454b76d701984927961a
Oriol Palau Requena <oriol.palau.requena@gmail.com>
2025-01-18 00:47:35 +0100

    write failing test for optimize

--- alpha_zero/alpha_zero.py
+++ alpha_zero/alpha_zero.py
@@ -7,71 +7,80 @@ from tinygrad import Tensor, nn
 from tinygrad.helpers import trange
 
 
-def self_learn(num_iterations: int, num_games: int, num_simulations: int):
-    f = ResNet()
+# def self_learn(num_iterations: int, num_games: int, num_simulations: int):
+#     f = ResNet()
+#
+#     for i in range(num_iterations):
+#         games = (self_play(f, num_simulations) for j in range(num_games))
+#         store(games, i)
+#         data = np.ndarray(move for game in games for move in game)
+#
+#         f = optimize(f, data)
+#         checkpoint(f, i)
+#
+#
+# def self_play(f: Model, num_simulations: int, v_resign: float = -1) -> np.ndarray:
+#     """
+#     For the first 30 moves of each game, the temperature is set to τ = 1; this selects moves proportionally
+#     to their visit count in MCTS, and ensures a diverse set of positions are encountered. For the remainder
+#     of the game, an infinitesimal temperature is used, τ → 0. Additional exploration is achieved by adding
+#     Dirichlet noise to the prior probabilities in the root node s0, this noise ensures that all moves may be
+#     tried, but the search may still overrule bad moves.
+#
+#     In order to save computation, clearly lost games are resigned. The resignation threshold vresign is selected
+#     automatically to keep the fraction of false positives (games that could have been won if AlphaGo had not
+#     resigned) below 5%. To measure false positives, we disable resignation in 10% of self-play games and play
+#     until termination.
+#     """
+#     env = Environment()
+#     s = State()
+#
+#     while not env.is_terminal(s) and v > v_resign:
+#         v, ps = mcts.search(node, f, env, num_simulations)
+#         record(s, ps)  # search should return these
+#         a, node = mcts.play(node)
+#
+#     # determine the outcome and add z to records
+
     
-    for i in range(num_iterations):
-        games = (self_play(f, num_simulations) for j in range(num_games))
-        store(games, i)
-        data = np.ndarray(move for game in games for move in game)
-
-        f = optimize(f, data)
-        checkpoint(f, i)
-
-
-def self_play(f: Model, num_simulations: int, v_resign: float = -1) -> np.ndarray:
-    """
-    For the first 30 moves of each game, the temperature is set to τ = 1; this selects moves proportionally
-    to their visit count in MCTS, and ensures a diverse set of positions are encountered. For the remainder
-    of the game, an infinitesimal temperature is used, τ → 0. Additional exploration is achieved by adding 
-    Dirichlet noise to the prior probabilities in the root node s0, this noise ensures that all moves may be
-    tried, but the search may still overrule bad moves. 
-
-    In order to save computation, clearly lost games are resigned. The resignation threshold vresign is selected
-    automatically to keep the fraction of false positives (games that could have been won if AlphaGo had not
-    resigned) below 5%. To measure false positives, we disable resignation in 10% of self-play games and play
-    until termination.
-    """
-    env = Environment()
-    s = State()
-
-    while not env.is_terminal(s) and v > v_resign:
-        v, ps = mcts.search(node, f, env, num_simulations)
-        record(s, ps)  # search should return these
-        a, node = mcts.play(node)
-
-    # determine the outcome and add z to records
-
-    
-def optimize(f: ResNet, data, num_steps=1000, batch_size=2048) -> ResNet:
+def optimize(f: ResNet, data, num_steps=1000, batch_size=2048, num_train_games=500_000, num_val_games=1) -> ResNet:
     """
     The batch-size is 32 per worker, for a total mini-batch size of 2,048. Each mini-batch of data
     is sampled uniformly at random from all positions from the most recent 500,000 games of self-play.
     It produces a new checkpoint every 1,000 training steps.
     """
     # TODO Structure data so it works
-    samples = Tensor.randint(num_steps, batch_size, high=x.shape[0])
-    x, pi, v  = data[-500_000, -1][samples]
-    x_test, pi_test, v_test = data[-1]
+    x, pi, v  = data
+    # x_test, pi_test, v_test = data[-num_val_games]
+    samples = Tensor.randint(num_steps, batch_size, high=x.shape[0], device="python")
+    x, pi, v = x[samples], pi[samples], v[samples]
     opt = nn.optim.Adam(nn.state.get_parameters(f))
+
+    print(f"x: {x[0].numpy()}")
+    print(f"pi: {pi[0].numpy()}")
+    print(f"v: {v[0].numpy()}")
 
     with Tensor.train():
         losses = []
         for i in range(samples.shape[0]):
             opt.zero_grad()
             p, z = f(x[i])
-            loss = - p.sparse_categorical_crossentropy(pi[i]) + (z - v) ** 2
+            print(f"p: {p.shape}, z: {z.shape}")
+            print(f"pi: {p.numpy()}")
+            print(f"v: {z.numpy()}")
+            loss = ((z - v[i]) ** 2 - p.sparse_categorical_crossentropy(pi[i], reduction="none")).mean()
+            print(f"loss: {loss.item()}")
             losses.append(loss.backward())
             opt.schedule_step()
 
-    with Tensor.test():
-        p, z = f(x_test)
-        test_p_loss = p.sparse_categorical_crossentropy(pi_test).mean()
-        test_v_loss = (z - v_test).pow(2).mean()
+    # with Tensor.test():
+        # p, z = f(x_test)
+        # test_p_loss = p.sparse_categorical_crossentropy(pi_test).mean()
+        # test_v_loss = (z - v_test).pow(2).mean()
 
     for i in (t:=trange(len(losses))): t.set_description(f"loss: {losses[i].item():6.2f}")
-    print(f"test_p_loss: {test_p_loss.item():5.2f}%")
-    print(f"test_v_loss: {test_v_loss.item():5.2f}%")
+    # print(f"test_p_loss: {test_p_loss.item():5.2f}%")
+    # print(f"test_v_loss: {test_v_loss.item():5.2f}%")
     return f
 
 
--- alpha_zero/tests/test_alpha_zero.py
+++ alpha_zero/tests/test_alpha_zero.py
@@ -1,6 +1,6 @@
 import unittest
 
-from alpha_zero import ResNet 
+from alpha_zero import ResNet, optimize
 
 from tinygrad import Tensor
 
@@ -12,6 +12,46 @@ class TestAlphaZero(unittest.TestCase):
 
         network(x)
 
+    def test_optimize(self):
+        import environments.tictactoe as env
+        import random
+
+        def simulate_game() -> tuple[list[env.State], list[int], list[float]]:
+            is_term = False
+            s = env.State()
+            states = []
+            actions = []
+            while not is_term:
+                states.append(s.board)
+
+                legal_actions = env.get_legal_actions(s)
+                a = random.choice(legal_actions)
+                actions.append(a)
+
+                s, r, is_term = env.transition(s, a)
+                s.turn()
+
+            if r == 0:
+                values = [0 for _ in states]
+            else:
+                values = []
+                for i in range(len(states)):
+                    values.append(r)
+                    r = -r
+                values = values[::-1]
+
+            return states, actions, values
+
+        x, pi, v = [], [], []
+        for i in range(100):
+            states, actions, values = simulate_game()
+            ps = [[1 if a==i else 0 for i in range(3*3+1)] for a in actions]
+            x.extend(states), pi.extend(ps), v.extend(values)
+
+        device = "python"
+        x, pi, v = Tensor(x, device=device), Tensor(pi, device=device), Tensor(v, device=device)
+        r = ResNet((3, 3), 2, 8, 2)
+        f = optimize(r, (x, pi, v), num_steps=100, batch_size=8)
 
 if __name__ == "__main__":
     unittest.main()

ae78301da00a070d02acbce26471086a86312e50
Oriol Palau Requena <oriol.palau.requena@gmail.com>
2025-02-09 16:29:46 +0100

    Add MCTS tests

--- alpha_zero/tests/test_mcts.py
+++ alpha_zero/tests/test_mcts.py
@@ -1,20 +1,37 @@
 import unittest
 
 from mcts import *
+import environments.tictactoe as env
+
+import numpy as np
+from tinygrad import Tensor
 
 
 class TestMCTS(unittest.TestCase):
-    def test_search(self):
-        class ConstantModel():
-            def __call__(self, x: Tensor) -> tuple[Tensor, Tensor]:
-                return Tensor([0.25]*4), Tensor([0.1])
+    class EnvironmentModel():
+        def __call__(self, x: Tensor) -> tuple[Tensor, Tensor]:
+            r = env.compute_reward(env.State(x.numpy()[0]))
+            return Tensor([1/9] * 9), Tensor(r)
 
-        model = ConstantModel()
-        import environments.tictactoe as env
-        root = MCTNode(parent=None, a=None, s=env.State())
-        
-        policy = search(root, model, env)
-        print(policy)
+    model = EnvironmentModel()
+
+    def test_search_winner(self):
+        board = np.array([[[0, 0, 0], [1, 1, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [1, 1, 0]]])
+        node = MCTNode(parent=None, a=None, s=env.State(board))
+        policy = search(node, self.model, env, 10)
+        self.assertEqual(policy.index(max(policy)), 5)
+
+    def test_search_blocker(self):
+        board = np.array([[[1, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [1, 1, 0]]])
+        node = MCTNode(parent=None, a=None, s=env.State(board))
+        policy = search(node, self.model, env, 40)
+        self.assertEqual(policy.index(max(policy)), 8)
+
+    def test_search_winner_in_three(self):
+        board = np.array([[[1, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 1, 1], [0, 0, 0], [0, 0, 0]]])
+        node = MCTNode(parent=None, a=None, s=env.State(board))
+        policy = search(node, self.model, env, 100)
+        self.assertEqual(policy.index(max(policy)), 6)
 
 
 if __name__ == "__main__":

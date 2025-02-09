9507671252cf18f73127158cd694e82311b1aecd
Oriol Palau Requena <oriol.palau.requena@gmail.com>
2025-02-09 16:31:48 +0100

    Fix MCTS exploring score

--- alpha_zero/mcts.py
+++ alpha_zero/mcts.py
@@ -3,6 +3,8 @@ from math import sqrt
 import logging
 import random
 
+from tinygrad import Tensor  # TODO remove MCTS dependency on tinygrad
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,13 +30,13 @@ class MCTNode():
 
     def is_leaf(self) -> bool:
         return len(self.children) == 0
+
+    def get_exploring_score(self, sqrt_sum_n: int, c_puct: float = 1) -> float:
+        return - self.q + c_puct * self.p * sqrt_sum_n / (1 + self.n)
     
     def get_best_child_to_explore(self, c_puct: float = 1) -> MCTNode:
         sqrt_sum_n = sqrt(sum(child.n for child in self.children))
-        return max(self.children, key=lambda x: x.q + x.upper_confidence_bound(sqrt_sum_n, c_puct))
-    
-    def upper_confidence_bound(self, sqrt_sum_n: float, c_puct: float = 1) -> float:
-        return c_puct * self.p * sqrt_sum_n / (1 + self.n)
+        return max(self.children, key=lambda x: x.get_exploring_score(sqrt_sum_n, c_puct))
     
     def get_best_child_to_play(self, temperature: float = 1) -> MCTNode:
         sum_n = sum(child.n ** (1 / temperature) for child in self.children)
@@ -61,26 +63,23 @@ def search(root: MCTNode, f, env, num_simulations: int = 800, temperature: float
     return root.get_policy(temperature, env.action_space_size)
 
 
-def select(node: MCTNode) -> MCTNode:
-    while not node.is_leaf():
+def select(node: MCTNode, env) -> MCTNode:
+    while not node.is_leaf(): # and not env.is_terminal(node.s): # TODO avoid MCTS exploring post terminal states!
         node = node.get_best_child_to_explore()
     return node
-
-
-from tinygrad import Tensor
 
 
 def expand_and_evaluate(leaf: MCTNode, f, env):
     # TODO queue nodes for evaluation with batch_size=8
     x = Tensor(leaf.s.board).reshape((1, 2, 3, 3))
-    ps, v = f(x) # TODO implement state representation as Tensor
-    logger.debug(f"v: {v.item()}, p: {ps.numpy().tolist()}")
-    for a, p in enumerate(ps.numpy().flatten()):
+    ps, v = f(x) # TODO model should take boards as numpy arrays
+    logger.debug(f"s: {leaf.s.board.tolist()}, v: {v.item()}, p: {ps.numpy().tolist()}")
+    for a, p in enumerate(ps.numpy().flatten()): # TODO avoid MCTS exploring post terminal states!
         if not env.is_legal(a, leaf.s): continue
         s, _, _ = env.transition(leaf.s, a)
         s.turn()
         child = MCTNode(leaf, a=a, s=s, to_play=(leaf.to_play + 1) % 2, p=p)
-        # logger.debug(f"adding child: {child}")
+        logger.debug(f"adding child: {child}")
         leaf.children.append(child)
     leaf.w = v.item()
 
